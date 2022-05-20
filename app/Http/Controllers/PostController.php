bac8505ec6ac90ab1c055e2e310d1f20306207ba
EC2 Default User <ec2-user@ip-172-31-26-88.ap-northeast-1.compute.internal>
2022-05-20 20:12:04 +0900

    カリキュラム9-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,26 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
 use App\Post;
 use App\Http\Requests\PostRequest; 
-use Illuminate\Http\Request;
+use App\Category;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginate()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
-    }
-
-    public function create()
-    {
-        return view('posts/create');
     }
 
     public function store(PostRequest $request, Post $post) 
@@ -48,5 +42,10 @@ class PostController extends Controller
     {
     $post->delete();
     return redirect('/');
+    }
+    
+    public function create(Category $category)
+    {
+    return view('posts/create')->with(['categories' => $category->get()]);;
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminatee\Databasee\Eloquente\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
@@ -12,10 +12,15 @@ class Post extends Model
     protected $fillable = [
     'title',
     'body',
+    'category_id'
 ];
 
-    public function getPaginateByLimit(int $limit_count = 10)
-    {
-     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-     }
+    public function getPaginateByLimit(int $limit_count = 5)
+{
+    return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
+      public function category()
+   {
+    return $this->belongsTo('App\Category');
+   }
 }
